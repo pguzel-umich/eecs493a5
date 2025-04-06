02f8fd1eb3e7e52d35ce58495fc597999fa24fc8
pguzel-umich <122236420+pguzel-umich@users.noreply.github.com>
2025-04-06 09:48:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,13 +7,41 @@ createApp({
       artist1: './img/1.jpg',
       artist2: './img/2.jpg',
 
-      searchQuery: "",
+      search_entry: "",
+      search_results: [],
+      search_count: 0,
     }
   },
 
   methods: {
-    searchArtist() {
-      console.log("search:", this.searchQuery);
+    searchBar() {
+      let search_val = this.search_entry.trim();
+
+      /* if (!search_val) {
+        console.log("no value entered");
+        return;
+      }*/
+
+      // console.log("search:", search_val);
+
+      search_val = encodeURIComponent(search_val);
+
+      let url_request = `https://itunes.apple.com/search?term=${search_val}&media=music`;
+
+      // console.log(url_request);
+
+      fetch(url_request).then(x => x.json()).then(y => {
+        console.log(y);
+
+        if (y.resultCount === 0) {
+          alert("No artist found.");
+          return;
+        }
+
+        this.search_results = y.results;
+        this.search_count = y.resultCount;
+      }
+      );
     }
 
   }
